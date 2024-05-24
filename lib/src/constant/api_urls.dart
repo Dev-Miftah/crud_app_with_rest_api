3f93a53b5c4b5239c18231f04ab395f4dfc04632
Miftah <miftahrahi24@gmail.com>
2024-05-24 18:40:47 +0600

    api urls

---
+++ lib/src/constant/api_urls.dart
@@ -0,0 +1,6 @@
+class ApiUrls{
+  static const String getProductListUrl = 'https://crud.teamrabbil.com/api/v1/ReadProduct';
+  static const String deleteProductUrl = 'https://crud.teamrabbil.com/api/v1/DeleteProduct/';
+  static const String updateProductUrl = 'https://crud.teamrabbil.com/api/v1/UpdateProduct/';
+  static const String addNewProductUrl = 'https://crud.teamrabbil.com/api/v1/CreateProduct';
+}
